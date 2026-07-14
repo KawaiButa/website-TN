a017c646972349298307479f3b7f527eae64b043
Kawaiibuta <21522441@gm.uit.edu.vn>
2026-07-15 01:57:21 +0700

    Final 3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -640,7 +640,7 @@ function App() {
           </div>
           <div>
 
-            <div className="section-card bento-campus" id="ban-do-khuon-vien" style={{ marginBottom: '15px' }}>
+            <div className="section-card bento-campus" id="ban-do-khuon-vien" style={{ marginBottom: '15px', minHeight: 0 }}>
               <h2 className="section-title">
                 {/* Icon Sơ đồ */}
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"><polygon points="3 6 9 3 15 6 21 3 21 18 15 21 9 18 3 21"></polygon><line x1="9" y1="3" x2="9" y2="18"></line><line x1="15" y1="6" x2="15" y2="21"></line></svg>
@@ -657,7 +657,7 @@ function App() {
                 />
               </div>
             </div>
-            <div className="section-card bento-contact" id="lien-he">
+            <div className="section-card bento-contact" id="lien-he" style={{ minHeight: 0}} >
               <h2 className="section-title">
                 {/* Icon Điện thoại */}
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path></svg>
